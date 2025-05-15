1f188dfe48089fa3feb9e622db2a1b1515098765
Emmanuel Montealegre Vargas <emontealegrev@udistrital.edu.co>
2025-05-14 20:28:08 -0500

    cambio sedentacion de la calculadora

--- CORTE 1/EVALUACION 1/CALCULADORA 2/CALCULADORA2.py
+++ CORTE 1/EVALUACION 1/CALCULADORA 2/CALCULADORA2.py
@@ -1,26 +1,40 @@
-def pedir_numeros():
-    a = float(input("Ingresa el primer número: "))
-    b = float(input("Ingresa el segundo número: "))
-    return a, b
 
-def suma():
+def suma(): 
+    def pedir_numeros():
+        a = float(input("Introduce el primer número: "))
+        b = float(input("Introduce el segundo número: "))
+        return a, b
+
     a, b = pedir_numeros()
     return a + b
+def resta():    
+    def pedir_numeros():
+        a = float(input("Introduce el primer número: "))
+        b = float(input("Introduce el segundo número: "))
+        return a, b
 
-def resta():
     a, b = pedir_numeros()
     return a - b
 
 def multiplicacion():
+    def pedir_numeros():
+        a = float(input("Introduce el primer número: "))
+        b = float(input("Introduce el segundo número: "))
+        return a, b
+
     a, b = pedir_numeros()
     return a * b
 
 def division():
+    def pedir_numeros():
+        a = float(input("Introduce el primer número: "))
+        b = float(input("Introduce el segundo número: "))
+        return a, b
+
     a, b = pedir_numeros()
-    if b != 0:
-        return a / b
-    else:
-        return "Error: División entre cero"
+    if b == 0:
+        return "Error: División por cero"
+    return a / b
 
 def calculadora():
     print("Calculadora con funciones")
